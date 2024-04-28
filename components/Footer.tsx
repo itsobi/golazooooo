6b1f445ee109dd3137344588f2117f8ef5c6b63c
itsobi <obiobialo@aol.com>
2024-04-28 11:57:37 -0500

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <div className="mt-auto p-2">
+      <p className="text-xs text-slate-300">
+        Brought to you by {''}
+        <Link
+          href="https://www.justobii.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-slate-400 hover:text-slate-500 hover:underline"
+        >
+          justobii.com
+        </Link>
+      </p>
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import { Search } from 'lucide-react';
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-6 px-4 border-b shadow-sm">
+      <p>Golazooooo</p>
+
+      <form className="flex items-center rounded-full px-4 py-2 space-x-2 lg:w-[500px] bg-slate-50">
+        <Search />
+        <input
+          placeholder="Search Golazooooo"
+          type="text"
+          className="outline-none bg-transparent"
+        />
+      </form>
+
+      {/* user button / signIn button */}
+      <p>button</p>
+    </header>
+  );
+}
