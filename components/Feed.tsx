fbbdcd06affe1d0010e1ef660834d31bea7a4007
itsobi <obiobialo@aol.com>
2024-04-28 21:30:03 -0500

    include bottom border on small screen

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,7 +2,7 @@ import PostForm from './PostForm';
 
 export default function Feed() {
   return (
-    <section className="col-span-full mx-4 md:mx-0 md:ml-4 md:col-span-6 xl:ml-0 xl:col-span-4 p-4 border border-b-0 rounded">
+    <section className="col-span-full mx-4 md:mx-0 md:ml-4 md:col-span-6 xl:ml-0 xl:col-span-4 p-4 border xl:border-b-0 rounded">
       <PostForm />
     </section>
   );
